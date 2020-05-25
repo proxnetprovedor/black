7a3462833254a78ca450d596b4c793cde48302cc
Fabricio Silva Sousa <fabriciolpu2@gmail.com>
2020-05-24 20:05:44 -0400

    [ADD] Factorys

---
+++ database/factories/CostumerFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Costumer;
+use Faker\Generator as Faker;
+
+$factory->define(Costumer::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
