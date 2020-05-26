3132b75d758e6ed31a51f1ef4f57c9ad93cb63e1
Fabricio Silva Sousa <fabriciolpu2@gmail.com>
2020-05-25 20:22:06 -0400

    [ADD] Controllers

---
+++ app/Http/Controllers/AccesPointController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AccesPoint;
+use Illuminate\Http\Request;
+
+class AccesPointController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AccesPoint  $accesPoint
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AccesPoint $accesPoint)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AccesPoint  $accesPoint
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AccesPoint $accesPoint)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AccesPoint  $accesPoint
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AccesPoint $accesPoint)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AccesPoint  $accesPoint
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AccesPoint $accesPoint)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/InstalationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Instalation;
+use Illuminate\Http\Request;
+
+class InstalationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Instalation  $instalation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Instalation $instalation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Instalation  $instalation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Instalation $instalation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Instalation  $instalation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Instalation $instalation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Instalation  $instalation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Instalation $instalation)
+    {
+        //
+    }
+}
