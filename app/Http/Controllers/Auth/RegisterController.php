f5452bc267547c6203df713ad57bdc8f2c493acc
Igor Alexander Silva <igor.alexander.silva@gmail.com>
2020-06-11 11:34:45 -0400

    [GERAl] Tradução do projeto

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,16 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\Tenant;
 use App\Providers\RouteServiceProvider;
 use App\Services\TenantService;
-use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Str;
-use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -54,13 +49,16 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'min:3', 'max:255'],
-            'email' => ['required', 'string', 'email', 'min:3', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'max:16', 'confirmed'],
-            'empresa' => ['required', 'unique:pgsql.providers.tenants,name', 'string', 'min:3', 'max:255',],
-            'cnpj' => ['required', 'unique:pgsql.providers.tenants', 'numeric', 'digits:14',],
-        ]);
+        return Validator::make(
+            $data,
+            [
+                'name' => ['required', 'string', 'min:3', 'max:255'],
+                'email' => ['required', 'string', 'email', 'min:3', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:6', 'max:16', 'confirmed'],
+                'empresa' => ['required', 'unique:pgsql.providers.tenants,name', 'string', 'min:3', 'max:255',],
+                'cnpj' => ['required', 'unique:pgsql.providers.tenants', 'numeric', 'digits:14',],
+            ],
+        );
     }
 
     /**
