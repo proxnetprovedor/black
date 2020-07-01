b2fdc5e4fde697b08db2fe66af118c4929c6528a
Igor Alexander Silva <igor.alexander.silva@gmail.com>
2020-07-01 17:22:14 -0400

    [ADD] Page 403

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,14 +1,16 @@
-@extends('layouts.app', ['page' => 'Não autorizado !', 'class' => 'page-header pricing-page header-filter', 'url' =>
+@extends('layouts.app', ['page' => 'Não autorizado !', 'class' => 'page-header error-page header-filter', 'url' =>
 '../../assets/img/clint-mckoy.jpg'])
 
 @section('content')
-<div class="row">
-    <div class="col-md-12">
-      <h1 class="title">403</h1>
-      <h2>Não autorizado :(</h2>
-      <h4>Oooops! Parece que você não tem permissão para acessar essa página.</h4>
+<div class="content-center">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="title">403</h1>
+            <h2>Não autorizado :(</h2>
+            <h4>Oooops! Parece que você não tem permissão para acessar essa página.</h4>
+        </div>
     </div>
-  </div>
+</div>
 </div>
 
 
