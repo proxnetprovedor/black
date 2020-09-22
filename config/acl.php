1ee606a5c768e3ae26dce8a263a7fb913060dac1
AlexandroGonSan <alexandrogonsan@outlook.com>
2020-09-22 18:55:14 -0400

    change admins

--- config/acl.php
+++ config/acl.php
@@ -4,9 +4,13 @@ return [
 
     'admins' => [
         'igor.alexander.silva@gmail.com',
-        'fabriciolpu2@gmail.com',
-        'danilovsdanilo@gmail.com',
-        'elderaraujo@gmail.com',
+        // 'fabriciolpu2@gmail.com',
+        // 'danilovsdanilo@gmail.com',
+        // 'elderaraujo@gmail.com',
+        'alexandrogonsanv2@outlook.com',
+        'satedilly@outlook.com',
+        'alexandrogonsan@outlook.com',
+        'alexandrogonsan@gmail.com'
     ],
 
 ];
